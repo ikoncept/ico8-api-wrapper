dc8a11ef0dcd3b45b6e1acce4c8b2262ede2415b
Daniel Källstrand Modig <daniel@ikoncept.se>
2022-03-01 11:12:54 +0100

    Add option to not use signed URLs

--- config/ico8-api-wrapper.php
+++ config/ico8-api-wrapper.php
@@ -4,4 +4,5 @@ return [
     'host' => env('ICO8_URL', 'http://api.ico8.test/v1'),
     'api_key' => env('ICO8_API_KEY', ''),
     'tenant_id' => env('ICO8_TENANT_ID', ''),
+    'sign_url' => env('ICO8_SIGN_URL', false),
 ];
